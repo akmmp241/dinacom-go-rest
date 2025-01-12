a81e4fc89eb9d83156df395cfa14720cf62cbf7b
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2025-01-12 09:04:17 +0700

    fix: can't register missing provider

--- db/migrations/20241221032821_create_users_table.up.sql
+++ db/migrations/20241221032821_create_users_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
-    provider ENUM('google') DEFAULT NULL
+    provider ENUM('email', 'google') DEFAULT NULL
 ) engine Innodb;
